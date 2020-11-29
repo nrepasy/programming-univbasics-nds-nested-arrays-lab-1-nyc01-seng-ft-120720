3cacae0cab76404f58c5109f691a8ec72ca0708b
nick repasy <nrepasy@gmail.com>
2020-11-29 21:20:41 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -57,7 +57,7 @@ def matrix_update(matrix, row, column, new_value)
     ORGANIC_PRODUCE
     ]
   
-  produce_storage[0][2] = "Onions"
+  produce_storage[0][2] = "Lemons"
   p produce_storage
   
   
